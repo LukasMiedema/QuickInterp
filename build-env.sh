498655b3d321858e7443cde16bd6ae758ea5493b
Lukas Miedema <lukas.miedema@gmail.com>
2020-06-24 02:12:25 +0200

    Build and runtime environment scripts

--- build-env.sh
+++ build-env.sh
@@ -1,3 +1,4 @@
+#export _JAVA_OPTIONS="-XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -Xverify:none"
 export _JAVA_OPTIONS="-Xverify:none"
 export CONF=linux-x86_64-normal-zero-slowdebug
 export JOBS=16
